0067e179dc4ea13303b88b1aec54bb3ac382f78b
Khairat Adesina <khairatadesina01@gmail.com>
2024-01-13 11:59:38 +0100

    add: making the search bar interactive

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,18 @@
+// Listen for changes on each input checkbox tag
+
+$(document).ready(function (){
+    $("input").on("change", function() {
+        let amenityIds = {};
+        if ($( "input:checked" )) {
+            amenityIds[$(this).data('id')] = $(this).data('name');
+        } else {
+            delete amenityIds[$(this).data('id')];
+        }
+        let newList = Object.values(amenityIds);
+        if (newList.length > 0) {
+          $('div.amenities > h4').text(Object.values(newList).join(', '));
+        } else {
+          $('div.amenities > h4').html('&nbsp;');
+        }
+    })
+})
